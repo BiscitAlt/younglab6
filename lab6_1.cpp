d4c7a894f4f9ba0bf3bab629cd1dcc0c80ebbbe0
BiscitAlt <thitiputt.pe@gmail.com>
2025-12-09 16:41:28 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,41 @@
 #include<iostream>
 using namespace std;
 
+void checkeven(int num[],int size){
+    int i = 0;
+    int count = 0;
+    while(i < (size-1)){
+        if((num[i] % 2) == 0){
+            count++;
+        }
+        i++;
+    }
+    cout << "#Even numbers = " << count << endl;
+}
+
+void checkodd(int num[],int size){
+    int i = 0;
+    int count = 0;
+    while(i < (size-1)){
+        if((num[i] % 2) != 0){
+            count++;
+        }
+        i++;
+    }
+    cout << "#Odd numbers = " << count << endl;
+}
+
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int i;
+    int size = 0;
+    int num[1000];
+    while(i != 0){
+        cout << "Enter an integer: ";
+        cin >> i;
+        num[size] = i;
+        size++;
+    }
+    checkeven(num,size);
+    checkodd(num,size);
     return 0;
 }
